7d2bd637825f6369c86d3cc0c69ccbbc615c5b61
Celine Tran <celinephoungtran@gmail.com>
2026-04-09 17:12:57 -0400

    comment out social image

--- theme.config.ts
+++ theme.config.ts
@@ -11,7 +11,7 @@ export default defineThemeConfig({
     description:
       'ANEA Beauty& Spa is a calming space designed for deep relaxation and gentle care. Our beauty and spa treatments focus on slow touch, comfort, and restoring balance for both skin. Quiet space, Gentle touch, No pressure sellin',
     author: 'Anea Beauty & Spa',
-    image: previewImage, // Can also be a string e.g. '/social-preview-image.png',
+    //image: previewImage, // Can also be a string e.g. '/social-preview-image.png',
   },
   colors: {
     primary: '#f0efe1',
