515029ee8e50d10a64107936573065b0e85abec4
Celine Tran <celinephoungtran@gmail.com>
2026-04-15 22:57:07 -0400

    fix collection creation

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,10 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content'
+
 
 // 2. Import loader(s)
-import { glob } from 'astro/loaders'
+
 
 // 3. Define your collection(s)
 
 // 4. Export a single `collections` object to register you collection(s)
+export const collections = { } 
